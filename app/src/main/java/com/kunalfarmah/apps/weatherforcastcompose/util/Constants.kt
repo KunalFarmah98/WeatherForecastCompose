586346786340c654d2027d7ac484f14acfa60719
Kunal Farmah <kunalfarmah98@gmail.com>
2023-04-09 19:57:56 +0530

    removed tokens

--- app/src/main/java/com/kunalfarmah/apps/weatherforcastcompose/util/Constants.kt
+++ app/src/main/java/com/kunalfarmah/apps/weatherforcastcompose/util/Constants.kt
@@ -2,6 +2,6 @@ package com.kunalfarmah.apps.weatherforcastcompose.util
 
 object Constants {
     const val BASE_URL = "https://api.openweathermap.org"
-    const val API_KEY_FREE = "f04809bc06735fc6814371524d6dd055"
-    const val API_KEY_PRO = "ed60fcfbd110ee65c7150605ea8aceea"
+    const val API_KEY_FREE = ""
+    const val API_KEY_PRO = ""
 }
