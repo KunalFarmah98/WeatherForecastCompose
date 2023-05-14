f7b98da8b290dbb74e2f31e5025a222cda77c054
Kunal Farmah <kunalfarmah98@gmail.com>
2023-05-14 14:38:18 +0530

    added scaffold and started UI

--- app/src/main/java/com/kunalfarmah/apps/weatherforcastcompose/screens/home/HomeScreen.kt
+++ app/src/main/java/com/kunalfarmah/apps/weatherforcastcompose/screens/home/HomeScreen.kt
@@ -1,53 +1,99 @@
 package com.kunalfarmah.apps.weatherforcastcompose.screens.home
 
+import android.annotation.SuppressLint
+import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.kunalfarmah.apps.weatherforcastcompose.data.DataOrException
+import com.kunalfarmah.apps.weatherforcastcompose.model.DailyForecast
 import com.kunalfarmah.apps.weatherforcastcompose.viewmodel.WeatherViewModel
+import com.kunalfarmah.apps.weatherforcastcompose.widgets.WeatherAppBar
 
 
 @Composable
 fun HomeScreen(navController: NavController) {
-    val weatherViewModel: WeatherViewModel  = hiltViewModel()
+    val weatherViewModel: WeatherViewModel = hiltViewModel()
     weatherViewModel.getCurrentForecast("delhi")
     weatherViewModel.getDailyForecast("delhi")
-    MainScaffold(weatherViewModel = weatherViewModel)
+    val currentData = weatherViewModel.currentData.collectAsState()
+    val dailyData = weatherViewModel.dailyData.collectAsState().value
+    MainScaffold(dailyData, navController)
+}
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScaffold(dailyData: DataOrException<DailyForecast, Boolean, Exception>, navController: NavController) {
+    Scaffold(topBar = {
+        WeatherAppBar(
+            title = "${dailyData.data?.city?.name}, ${dailyData.data?.city?.country ?: ""}",
+            navController = navController,
+            elevation = 5.dp
+        ) {
+            Log.d("Home", "MainScaffold: button clicked")
+        }
+    }) {
+        MainContent(dailyData, paddingValues = it)
+    }
 }
 
 @Composable
-fun MainScaffold(weatherViewModel: WeatherViewModel) {
+fun MainContent(dailyData:DataOrException<DailyForecast, Boolean, Exception>, paddingValues: PaddingValues) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(
+                top = paddingValues.calculateTopPadding(),
+                start = 4.dp,
+                end = 4.dp,
+                bottom = paddingValues.calculateBottomPadding()
+            ),
+        verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally
+    ) {
 
-    val dailyDataFlow = weatherViewModel.dailyData.collectAsState().value
+        Text(
+            text = "Nov 29",
+            style = MaterialTheme.typography.labelLarge,
+            color = Color.Black,
+            fontWeight = FontWeight.SemiBold,
+            modifier = Modifier.padding(6.dp)
+        )
 
-    val currentDataFlow = weatherViewModel.currentData.collectAsState().value
+        Surface(
+            modifier = Modifier
+                .padding(4.dp)
+                .size(200.dp),
+            shape = CircleShape,
+            color = Color(0xFFFFC400)
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = "56", style = MaterialTheme.typography.headlineMedium, fontWeight = FontWeight.ExtraBold)
+                Text(text = "Snow", fontStyle = FontStyle.Italic)
 
-    Column(modifier = Modifier.fillMaxSize()) {
-
-        if(dailyDataFlow.loading==true)
-            Text(text = "loading daily forecast as flow")
-        else {
-            var weather = ""
-            dailyDataFlow.data?.list?.forEach {
-                weather+=it.weather.toString()
             }
-            Text(text = weather)
         }
-        Spacer(modifier = Modifier.height(10.dp))
-        if(currentDataFlow.loading==true)
-            Text(text = "loading current forecast as flow")
-        else
-            Text(text = currentDataFlow.data.toString())
     }
-
-
-
-
 }
---
+++ app/src/main/java/com/kunalfarmah/apps/weatherforcastcompose/widgets/WeatherAppBar.kt
@@ -0,0 +1,76 @@
+package com.kunalfarmah.apps.weatherforcastcompose.widgets
+
+import androidx.compose.foundation.clickable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.kunalfarmah.apps.weatherforcastcompose.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun WeatherAppBar(
+    title: String = "",
+    icon: ImageVector? = null,
+    isMainScreen: Boolean = true,
+    elevation: Dp = 0.dp,
+    navController: NavController,
+    onAddActionClicked: () -> Unit = {},
+    onButtonClicked: () -> Unit = {}
+) {
+    CenterAlignedTopAppBar(title = {
+        Text(
+            text = title, color = MaterialTheme.colorScheme.secondary,
+            style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 20.sp)
+        )
+    }, actions = {
+        if (isMainScreen) {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(imageVector = Icons.Default.Search, contentDescription = "Search")
+            }
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(imageVector = Icons.Default.MoreVert, contentDescription = "More")
+            }
+        }
+    }, navigationIcon = {
+        if (isMainScreen) {
+            if (icon == null) {
+                IconButton(onClick = {onButtonClicked.invoke()}) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.sun),
+                        contentDescription = "app icon"
+                    )
+                }
+            } else {
+                Icon(imageVector = icon,
+                    contentDescription = "app icon",
+                    tint = MaterialTheme.colorScheme.onSecondary,
+                    modifier = Modifier.clickable {
+                        onButtonClicked.invoke()
+                    }
+                )
+            }
+        } else {
+            IconButton(onClick = { navController.popBackStack() }) {
+                Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "back")
+            }
+        }
+    })
+
+}
